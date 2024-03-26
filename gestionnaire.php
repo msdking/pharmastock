8fd66a1d674cda59633f3d4486172f5f6d337ecb
msdking <51804799+msdking@users.noreply.github.com>
2024-03-26 20:19:13 +0100

    Add files via upload

--- gestionnaire.php
+++ gestionnaire.php
@@ -1,14 +1,10 @@
-
 <?php
-use Illuminate\Support\Facades\Hash;
-use App\Models\Gestionnaire;
+namespace App\Models;
 
-// Example of storing a new user with a hashed password
-$password = 'messaoudi2024';
-$hashedPassword = Hash::make($password);
+use Illuminate\Database\Eloquent\Model;
 
-$user = new Gestionnaire();
-$user->email = 'msdrabah13@gmail.com';
-$user->password = $hashedPassword;
-// Set other user attributes...
-$user->save();
+class Gestionnaire extends Model
+{
+    protected $table = 'gestionnaire';
+    protected $fillable = ['nom', 'prenom', 'email', 'password', 'tel', 'address', 'photo', 'civilite'];
+}
