5ad0377692d05e3a36c0f8306dcda752635f88cc
msdking <51804799+msdking@users.noreply.github.com>
2024-04-02 07:46:44 +0200

    Add files via upload

---
+++ pharmastock.blade.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Pages / Login - NiceAdmin Bootstrap Template</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;600&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <link href="assets/scss/card.css" rel="stylesheet">
+
+   <!-- =======================================================
+  * Template Name: NiceAdmin
+  * Updated: Mar 10 2024 with Bootstrap v5.3.3
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  <style>
+ body {
+    position: relative;
+   /* background-image: url('assets/img/backpharma.png');
+   */
+   background-color: black;
+   background-size: cover;
+    background-position: center;
+}
+
+body::before {
+    content: "";
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 500px;
+    background-color: rgba(0, 0, 0, 0.5); /* Adjust the opacity (0.5) to make it darker or lighter */
+}
+
+
+body::before {
+    content: "";
+    position: absolute;
+    top: 10;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    /*background-image: url('assets/img/backpharma.png');*/
+    background-size: cover;
+    background-position: center;
+    filter: brightness(70%); /* Adjust the brightness level (70%) as needed */
+    z-index: -1; /* Ensure the background is behind the content */
+}
+
+.bg-video {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      z-index: -1;
+      overflow: hidden;
+    }
+    .bg-video video {
+      width: 100%;
+      height: 100%;
+      object-fit: cover; /* Ensures the video covers the entire container */
+    }
+  </style>
+</head>
+
+<body  >
+<div class="bg-video">
+  <video autoplay muted loop>
+    <source src="assets/img/back.mp4" type="video/mp4">
+
+  </video>
+</div>
+
+    <main>
+        <div class="container">
+      <!-- Logo and Three Asides Container -->
+            <div class="row justify-content-center align-items-center">
+            <!-- Logo -->
+                <div class="d-flex justify-content-center py-4">
+                    <a href="index.html" class=" d-flex align-items-center w-auto m-10 ">
+                        <img src="assets/img/logo.png" alt="" class="white-logo">
+                    </a>
+                </div><!-- End Logo -->
+
+                <div class="container ">
+                <section >
+  <div class="row ">
+    <div class="col-md-4 col-sm-6 col-xs-12">
+      <div class="card">
+        <div class="cover item-a">
+          <h1>Client</h1>
+          <span class="price">
+          <i class="bi bi-circle-fill text-white"></i>
+
+          </span>
+          <div class="card-back">
+            <a href="#">Open your profile</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 col-sm-6 col-xs-12">
+      <div class="card">
+        <div class="cover item-b">
+          <h1>Admin</h1>
+          <span class="price">
+          <i class="bi bi-circle-fill text-white"></i>
+
+          </span>
+                    <div class="card-back">
+            <a href="#">Open your profile</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-4 col-sm-6 col-xs-12">
+      <div class="card">
+        <div class="cover item-c">
+          <h1>Manager</h1>
+          <span class="price">
+          <i class="bi bi-circle-fill text-white"></i>
+
+          </span>
+                    <div class="card-back">
+            <a href="#">Open your profile</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+            </div><!-- End Logo and Three Asides Container -->
+        </div>
+    </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
