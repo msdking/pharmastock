fe30e6468b8f67f206ca58ce6a363c66a77e5c4c
Ayoub <163001554+youbi102@users.noreply.github.com>
2024-05-29 23:56:53 +0100

    Add files via upload

--- Gestionnaire.php
+++ Gestionnaire.php
@@ -2,17 +2,25 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
+use Illuminate\Notifications\Notifiable;
 
 class Gestionnaire extends Model implements Authenticatable
 {
-    use AuthenticatableTrait;
-    public $timestamps = false;
-
-    protected $table = 'gestionnaire';
-    protected $fillable = ['id','email', 'password','nom','prenom'];
+    use HasFactory,AuthenticableTrait,Notifiable;
+    protected $guard = 'gestionnaires';
+    protected $fillable = [
+        'nom',
+        'prenom',
+        'email',
+        'password',
+        'tel',
+        'address',
+        'photo',
+        'civilite',
+        'type',
+    ];
 }
-
-
